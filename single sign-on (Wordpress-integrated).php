bb03bd5cf1cd77bfd1595e0dce1ac04a7dfe1f4d
EleRam <eleram.com@gmail.com>
2013-12-20 18:15:19 +0330

    Update single sign-on (Wordpress-integrated).php

--- single sign-on (Wordpress-integrated).php
+++ single sign-on (Wordpress-integrated).php
@@ -1,41 +1,48 @@
 function post_meta_who($post, $class)
+{
+	if (isset($post['who']))
+	{
+		// Default Wordpress 'Administrator's username is 'Admin'. Change it in below line if you have choosed another name.
+		$admin_userlogin = 'Admin';
+		
+		$this->output('<span class="' . $class . '-who">');
+		$user = get_user_by( 'login', $post['who']['data'] == 'Admin' ? $post['who']['data'] : $admin_userlogin );
+		
+		if (strlen(@$post['who']['prefix'])) $this->output('<span class="' . $class . '-who-pad">' . $post['who']['prefix'] . '</span>');
+		if (isset($post['who']['data']))
 		{
-			if (isset($post['who']))
-			{
-				$this->output('<span class="' . $class . '-who">');
-				if (strlen(@$post['who']['prefix'])) $this->output('<span class="' . $class . '-who-pad">' . $post['who']['prefix'] . '</span>');
-				if (isset($post['who']['data'])) $this->output('<span class="' . $class . '-who-data">' . $post['who']['data'] . '</span>');
-				if (isset($post['who']['title'])) $this->output('<span class="' . $class . '-who-title">' . $post['who']['title'] . '</span>');
-				if (current_user_can('administrator'))
-				{
-					$this->output(" (مدیر کل سایت)");
-				}
-				elseif (current_user_can('subscriber'))
-				{
-					$this->output(" (کاربر معمولی)");
-				}
-				elseif (current_user_can('editor'))
-				{
-					$this->output(" (ناظم)");
-				}
-				elseif (current_user_can('contributor'))
-				{
-					$this->output(" (متخصص)");
-				}
-				else
-				{
-					$this->output(" (کاربر معمولی)");
-				}
-		
-				if (isset($post['who']['points']))
-				{
-					$post['who']['points']['prefix'] = '(' . $post['who']['points']['prefix'];
-					$post['who']['points']['suffix'].= ')';
-					$this->output_split($post['who']['points'], $class . '-who-points');
-				}
-		
-				if (strlen(@$post['who']['suffix'])) $this->output('<span class="' . $class . '-who-pad">' . $post['who']['suffix'] . '</span>');
-				$this->output('</span>');
-			}
+			$this->output('<span class="' . $class . '-who-data">' . $post['who']['data']. '</span>');
 		}
+		if (isset($post['who']['title'])) $this->output('<span class="' . $class . '-who-title">' . $post['who']['title'] . '</span>');
+		if ($user->roles[0] == 'administrator')
+		{
+			$this->output(" (مدیر کل سایت)");
+		}
+		elseif ($user->roles[0] == 'subscriber')
+		{
+			$this->output(" (کاربر معمولی)");
+		}
+		elseif ($user->roles[0] == 'editor')
+		{
+			$this->output(" (ناظم)");
+		}
+		elseif ($user->roles[0] == 'contributor')
+		{
+			$this->output(" (متخصص)");
+		}
+		else
+		{
+			$this->output(" (کاربر معمولی)");
+		}
+
+		if (isset($post['who']['points']))
+		{
+			$post['who']['points']['prefix'] = '(' . $post['who']['points']['prefix'];
+			$post['who']['points']['suffix'].= ')';
+			$this->output_split($post['who']['points'], $class . '-who-points');
+		}
+
+		if (strlen(@$post['who']['suffix'])) $this->output('<span class="' . $class . '-who-pad">' . $post['who']['suffix'] . '</span>');
+		$this->output('</span>');
+	}
 }
